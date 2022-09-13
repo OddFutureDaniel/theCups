661e50661d01803229bb97a79b7eaeb58e4a9235
OddFutureDaniel <danielmatt@hotmail.co.uk>
2022-09-13 19:21:43 +0100

    stop sticky nav and menu divs

---
+++ divScript.js
@@ -0,0 +1,65 @@
+var $ = window.jQuery;
+/*  show 1 - hide rest  */
+
+$('.show-1-yes').click(function() {
+    $('#target-1').show(500);
+    $('#target-2').hide(500);
+    $('#target-3').hide(500);
+    $('#target-4').hide(500);
+    $('#target-5').hide(500);
+    $('#target-6').hide(500);
+});
+/*  show 2 - hide rest  */
+
+$('.show-2-yes').click(function() {
+    $('#target-2').show(500);
+    $('#target-1').hide(500);
+    $('#target-3').hide(500);
+    $('#target-4').hide(500);
+    $('#target-5').hide(500);
+    $('#target-6').hide(500);
+});
+
+/*  show 3 - hide rest  */
+
+$('.show-3-yes').click(function() {
+    $('#target-3').show(500);
+    $('#target-1').hide(500);
+    $('#target-2').hide(500);
+    $('#target-4').hide(500);
+    $('#target-5').hide(500);
+    $('#target-6').hide(500);
+});
+
+/*  show 4 - hide rest  */
+
+$('.show-4-yes').click(function() {
+    $('#target-4').show(500);
+    $('#target-1').hide(500);
+    $('#target-2').hide(500);
+    $('#target-3').hide(500);
+    $('#target-5').hide(500);
+    $('#target-6').hide(500);
+});
+
+/*  show 5 - hide rest  */
+
+$('.show-5-yes').click(function() {
+    $('#target-5').show(500);
+    $('#target-1').hide(500);
+    $('#target-2').hide(500);
+    $('#target-3').hide(500);
+    $('#target-4').hide(500);
+    $('#target-6').hide(500);
+});
+
+/*  show 6 - hide rest  */
+
+$('.show-6-yes').click(function() {
+    $('#target-6').show(500);
+    $('#target-1').hide(500);
+    $('#target-2').hide(500);
+    $('#target-3').hide(500);
+    $('#target-4').hide(500);
+    $('#target-5').hide(500);
+});
