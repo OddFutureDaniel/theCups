53b2323d51152f938a48e887e6f03a0e20d5446b
OddFutureDaniel <danielmatt@hotmail.co.uk>
2022-09-19 20:47:25 +0100

    button options fixed

--- divScript.js
+++ divScript.js
@@ -1,88 +1,99 @@
 var $ = window.jQuery;
 /*  show menu 1 - hide rest  */
 
-$('.show-1-yes').click(function() {
-    $('#target-1').show(500);
-    $('#target-2').hide(500);
-    $('#target-3').hide(500);
-    $('#target-4').hide(500);
-    $('#target-5').hide(500);
-    $('#target-6').hide(500);
+$(".show-1-yes").click(function () {
+  $("#target-1").show(500);
+  $("#target-2").hide(500);
+  $("#target-3").hide(500);
+  $("#target-4").hide(500);
+  $("#target-5").hide(500);
+  $("#target-6").hide(500);
 });
 /*  show menu 2 - hide rest  */
 
-$('.show-2-yes').click(function() {
-    $('#target-2').show(500);
-    $('#target-1').hide(500);
-    $('#target-3').hide(500);
-    $('#target-4').hide(500);
-    $('#target-5').hide(500);
-    $('#target-6').hide(500);
+$(".show-2-yes").click(function () {
+  $("#target-2").show(500);
+  $("#target-1").hide(500);
+  $("#target-3").hide(500);
+  $("#target-4").hide(500);
+  $("#target-5").hide(500);
+  $("#target-6").hide(500);
 });
 
 /*  show menu 3 - hide rest  */
 
-$('.show-3-yes').click(function() {
-    $('#target-3').show(500);
-    $('#target-1').hide(500);
-    $('#target-2').hide(500);
-    $('#target-4').hide(500);
-    $('#target-5').hide(500);
-    $('#target-6').hide(500);
+$(".show-3-yes").click(function () {
+  $("#target-3").show(500);
+  $("#target-1").hide(500);
+  $("#target-2").hide(500);
+  $("#target-4").hide(500);
+  $("#target-5").hide(500);
+  $("#target-6").hide(500);
 });
 
 /*  show menu 4 - hide rest  */
 
-$('.show-4-yes').click(function() {
-    $('#target-4').show(500);
-    $('#target-1').hide(500);
-    $('#target-2').hide(500);
-    $('#target-3').hide(500);
-    $('#target-5').hide(500);
-    $('#target-6').hide(500);
+$(".show-4-yes").click(function () {
+  $("#target-4").show(500);
+  $("#target-1").hide(500);
+  $("#target-2").hide(500);
+  $("#target-3").hide(500);
+  $("#target-5").hide(500);
+  $("#target-6").hide(500);
 });
 
 /*  show menu 5 - hide rest  */
 
-$('.show-5-yes').click(function() {
-    $('#target-5').show(500);
-    $('#target-1').hide(500);
-    $('#target-2').hide(500);
-    $('#target-3').hide(500);
-    $('#target-4').hide(500);
-    $('#target-6').hide(500);
+$(".show-5-yes").click(function () {
+  $("#target-5").show(500);
+  $("#target-1").hide(500);
+  $("#target-2").hide(500);
+  $("#target-3").hide(500);
+  $("#target-4").hide(500);
+  $("#target-6").hide(500);
 });
 
 /*  show menu 6 - hide rest  */
 
-$('.show-6-yes').click(function() {
-    $('#target-6').show(500);
-    $('#target-1').hide(500);
-    $('#target-2').hide(500);
-    $('#target-3').hide(500);
-    $('#target-4').hide(500);
-    $('#target-5').hide(500);
+$(".show-6-yes").click(function () {
+  $("#target-6").show(500);
+  $("#target-1").hide(500);
+  $("#target-2").hide(500);
+  $("#target-3").hide(500);
+  $("#target-4").hide(500);
+  $("#target-5").hide(500);
 });
 
 function dropFunc() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
 
-  function selected(clicked){
-    document.getElementById(clicked).classList.toggle("select");
+jQuery($ => {
+    let $productButtons = $('button.productimg').on('click', e => {
+      let $button = $(e.currentTarget);
+      $productButtons.not($button).removeClass('select');
+      $button.toggleClass('select');
+    });
+  });
 
-  }
+  jQuery($ => {
+    let $productButtons = $('button.productimgTopping').on('click', e => {
+      let $button = $(e.currentTarget);
+      $productButtons.not($button).removeClass('select');
+      $button.toggleClass('select');
+    });
+  });
